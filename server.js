5c4172c783bb24ecc54e8c8f802950418bca232d
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-17 17:44:27 -0300

    feat: criando rotas

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import http from "http";
 
 const PORT = 3000;
 
+// criando rotas
+const rotas = {
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+}
+
 // criando o servidor
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]);
 })
 
 server.listen(PORT, () => {
