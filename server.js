a27ce95e37c022a1f2722738f76ec0487a21d4a8
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-17 17:22:25 -0300

    feat: criando o servidor

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import http from "http";
 
 const PORT = 3000;
 
+// criando o servidor
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
     res.end("Curso de Node.js");
