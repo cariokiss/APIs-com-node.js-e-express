063efc6382297c8edbe70b8508090ae7fd490b96
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-19 15:25:20 -0300

    feat: criando a rota livros

--- server.js
+++ server.js
@@ -1,20 +1,6 @@
-// import http from "http";
 import app from './src/app.js';
 
 const PORT = 3000;
-
-// criando rotas
-// const rotas = {
-//     "/": "Curso de Node.js",
-//     "/livros": "Entrei na rota livros",
-//     "/autores": "Entrei na rota autores"
-// }
-
-// criando o servidor
-// const server = http.createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "text/plain" });
-//     res.end(rotas[req.url]);
-// })
 
 app.listen(PORT, () => {
     console.log("servidor escutando!")
