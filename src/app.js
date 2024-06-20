0fc009dcee262b26f0dbf1767a7a825e9e424c2e
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-20 11:10:19 -0300

    feat: conectando a API com o banco MongoDB

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import express from 'express';
+import conectaNaDatabase from './config/dbConnect.js';
+
+// estabelece a conexão com o banco de dados
+const conexao = await conectaNaDatabase();
+
+// caso conexão falhe
+conexao.on('error', (erro) => {
+    console.error('erro de conexão', erro);
+})
+
+
+// caso conexão seja bem sucedida
+conexao.once('open', () => {
+    console.log('Conexão com o banco feita com sucesso');
+})
 
 const app = express();
 app.use(express.json());
