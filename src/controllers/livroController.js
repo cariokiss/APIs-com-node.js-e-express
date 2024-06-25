637ef84116af1062b1e5863a1bf1f12c3061d8bf
Victor Ozoterio <victorozoterio@gmail.com>
2024-06-25 16:56:33 -0300

    feat: criando os métodos get por id e update

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -10,6 +10,16 @@ class LivroController {
         }
       };
 
+    static async listaLivroPorId (req, res) {
+      try {
+        const id = req.params.id
+        const livroEncontrado = await livro.findById(id);
+        res.status(200).json(livroEncontrado);
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na requisição do livro` });
+      }
+    }
+
     static async cadastrarLivro (req, res) {
         try {
             const novoLivro = await livro.create(req.body);
@@ -17,6 +27,16 @@ class LivroController {
         } catch (erro) {
             res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`})
         }
+    };
+
+    static async atualizarLivro (req, res) {
+      try {
+        const id = req.params.id
+        await livro.findByIdAndUpdate(id, req.body);
+        res.status(200).json({ message: 'livro atualizado' });
+      } catch (erro) {
+        res.status(500).json({ message: `${erro.message} - falha na atualização` });
+      }
     }
 };
 
